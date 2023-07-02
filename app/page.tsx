88c594092f3ede1f1b2c9e555f3ec9f0fc07d892
rohitGupta2022 <pta.rohit28@gmail.com>
2023-07-02 19:19:40 +0530

    adding the todo card

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import AryaForm from "@/components/AryaForm";
 import Board from "@/components/Board";
 import { Header } from "@/components/Header";
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
 
       {/* Board */}
       <Board />
+      {/* <AryaForm /> */}
 
     </main>
   )
--- components/Column.tsx
+++ components/Column.tsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import { Draggable, Droppable } from 'react-beautiful-dnd'
+import { PlusCircleIcon } from "@heroicons/react/24/solid";
+import React from "react";
+import { Draggable, Droppable } from "react-beautiful-dnd";
+import TodoCard from "./TodoCard";
 
 type ColumnProps = {
-  id: TypedColumn,
-  todos: Todo[],
-  index: number,
-}
+  id: TypedColumn;
+  todos: Todo[];
+  index: number;
+};
+
+const idToColumnText: {
+  [key in TypedColumn]: string;
+} = {
+  todo: "To Do",
+  inprogress: "In Progress",
+  done: "Done",
+};
+console.log(idToColumnText);
 
 function Column(props: ColumnProps) {
-  const { id, todos, index } = props
+  const { id, todos, index } = props;
   return (
     <Draggable draggableId={id} index={index}>
       {(provided) => (
@@ -17,27 +28,55 @@ function Column(props: ColumnProps) {
           {...provided.dragHandleProps}
           ref={provided.innerRef}
         >
+          {/* Render droppable todos in the column */}
           <Droppable droppableId={index.toString()} type='card'>
             {(provided, snapshot) => (
-
               <div
                 {...provided.droppableProps}
                 ref={provided.innerRef}
-                className={`p-2 rounded-2xl shadow-sm ${snapshot.isDraggingOver ? 'bg-green-200' : 'bg-white/50'}`}
+                className={`p-2 rounded-2xl shadow-sm ${snapshot.isDraggingOver ? "bg-green-200" : "bg-white/50"
+                  }`}
               >
-                <h2>
-                  {id}
+                <h2 className='flex justify-between font-bold text-xl p-2 '>
+                  {idToColumnText[id]}
+                  <span className='text-gray-500 bg-gray-200 rounded-full px-2 py-1 text-sm font-normal'>
+                    {todos.length}
+                  </span>
                 </h2>
-
+                <div className="space-y-2">
+                  {todos.map((todo, index) => (
+                    <Draggable
+                      key={todo.$id}
+                      draggableId={todo.$id}
+                      index={index}
+                    >
+                      {(provided) => (
+                        <TodoCard
+                          todo={todo}
+                          index={index}
+                          id={id}
+                          innerRef={provided.innerRef}
+                          draggableProps={provided.draggableProps}
+                          dragHandleProps={provided.dragHandleProps}
+                        />
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                  <div className="flex items-end justify-end p-2">
+                    <button className="text-green-500 hover:text-green-600">
+                      <PlusCircleIcon className="h-10 w-10" />
+                    </button>
+                  </div>
+                </div>
               </div>
             )}
-          </Droppable >
+          </Droppable>
           {/* {render droppable todos in the column} */}
         </div>
       )}
-
     </Draggable>
-  )
+  );
 }
 
-export default Column
+export default Column;
