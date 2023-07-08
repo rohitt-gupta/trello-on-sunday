b5e55883aa6465acd12776dec10a2ab81b0b4fe6
rohitGupta2022 <pta.rohit28@gmail.com>
2023-07-08 13:17:55 +0530

    fixed the board error in board.js

--- components/Board.tsx
+++ components/Board.tsx
@@ -104,20 +104,15 @@ function Board() {
 
   return (
     <DragDropContext onDragEnd={handleOnDragEnd}>
-      <Droppable droppableId='board' direction='horizontal' type='column'>
+      <Droppable droppableId="board" direction="horizontal" type="column">
         {(provided) => (
           <div
-            className='grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto '
+            className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto"
             {...provided.droppableProps}
             ref={provided.innerRef}
           >
-            {Array.from(board.columns.entries()).map(([id, column], index) => (
-              <Column
-                key={id}
-                id={id as TypedColumn}
-                todos={(column as Column).todos}
-                index={index}
-              />
+            {[...board.columns.entries()].map(([id, column], index) => (
+              <Column key={id} id={id} todos={column.todos} index={index} />
             ))}
           </div>
         )}
